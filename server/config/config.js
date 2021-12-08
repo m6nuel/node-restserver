961900aff7bd64caded6574ac812ba79bb9f456c
Jesus Delgado <m6nuel@gmail.com>
2021-12-08 16:01:42 -0300

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://m6nuel:VwWPLx8cvqStGqR5@cluster0.8yyas.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
